d8fa7ecf14938f645d87dbce84c7c60f2a8555f4
Michael Tvrdík <tvrdikmichael400@gmail.com>
2025-09-26 00:25:49 +0200

    Delete server directory

--- server/src/Components/AcceptFilteredData.js
+++
@@ -1,3 +0,0 @@
-export default function acceptedProducts(data) {
-    return data;
-}
--- server/src/Components/SplitTitle.js
+++
@@ -1,9 +0,0 @@
-// Function to truncate a title string if it exceeds 30 characters
-export default function splitTitle(title) {
-  // Return empty string if input is not a string
-  if (typeof title !== "string") return "";
-
-  // If title is longer than 30 characters, return first 30 chars + "..."
-  // Otherwise, return the full title as is
-  return title.length > 30 ? `${title.slice(0, 30)}...` : title;
-}
